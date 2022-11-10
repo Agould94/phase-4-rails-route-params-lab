59098f3b14f26a1617350ac58c2684c4535e0658
Agould94 <77901585+Agould94@users.noreply.github.com>
2022-11-09 23:30:08 -0500

    commit tree e9a1a5eca3157532ed518964aca958f282b10c79

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    students = if params[:name]
+      Student.by_name(params[:name]) 
+    else 
+      Student.all 
+    end
+      render json: students
+    #byebug
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+    #byebug
   end
 
 end
